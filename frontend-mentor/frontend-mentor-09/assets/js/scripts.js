d05770eddfb9bf45cc924f544fb7b10373682720
William <will.mart.tria.0304@gmail.com>
2026-01-15 03:52:12 +0100

    changes in the new project

--- frontend-mentor/frontend-mentor-09/assets/js/scripts.js
+++ frontend-mentor/frontend-mentor-09/assets/js/scripts.js
@@ -1,1 +1,42 @@
-window.addEventListener("DOMContentLoaded", () => { });
+window.addEventListener("DOMContentLoaded", () => {
+  const playerScore = document.querySelector("#player-score");
+  const houseScore = document.querySelector("#house-score");
+  const optionContainer = document.querySelector("#option-container")
+  const showResults = document.querySelector("#show-results");
+  const showRules = document.querySelector("#show-rules");
+  const btnRules = document.querySelector("#btn-rules");
+  const arrOptions = ["paper","scissors","rock"];
+
+  const housePlayer = () => {
+    return arrOptions[Math.floor(Math.random() * arrOptions.length,2)]
+  }
+
+  const deliverWinner = (player,house) => {
+    if (player === "rock" && house === "scissors") {
+      return true;
+    }
+    if (player === "scissors" && house === "paper") {
+      return true;
+    }
+
+    if (player === "paper" && house === "rock") {
+      return true
+    }
+
+    if (player === house) {
+      return;
+    }
+    return false;
+  }
+
+  const getWinner = playerPulse => {
+    let house = housePlayer();
+    let player = playerPulse;
+    return deliverWinner(player,house);
+  }
+
+  optionContainer.addEventListener("click", (e) => {
+    let pulsedElement = e.target.closest(".wrap__img");
+    console.log(getWinner(pulsedElement.id));
+  });
+});
