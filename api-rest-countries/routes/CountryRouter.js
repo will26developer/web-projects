d0216e55b1b628e08b7a45816adf42adbc2c4578
will26developer <will.mart.tria.0304@gmail.com>
2026-02-15 19:00:37 +0100

    new change in repository

--- api-rest-countries/routes/CountryRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const CountryController = require('../controllers/CountryController');
-
-const router = express.Router();
-
-// Route to get all countries
-router.get('/countries', CountryController.getAllCountries);
-
-module.exports = router;
