afd56c56c49600d32d822ad5d359b55682e044a2
will26developer <will.mart.tria.0304@gmail.com>
2026-03-26 16:13:47 +0100

    new changes in frontend mentor 03

--- frontend-mentor/frontend-mentor-03/assets/js/script.js
+++ frontend-mentor/frontend-mentor-03/assets/js/script.js
@@ -1,19 +1,5 @@
-import listenersElements from "./listeners/listenersElements.js";
-import router from "./router/router.js";
-import uiFunctions from "./services/uiFunctions.js";
-import utilFunctions from "./services/utilFunctions.js";
+
 
 window.addEventListener("DOMContentLoaded",() => {
 
-  window.addEventListener("hashchange",router);
-
-  const initApp = async () => {
-    router();
-    uiFunctions.setDefaultTheme();
-    await utilFunctions.getApiData();
-    uiFunctions.renderCountries();
-    listenersElements();
-  }
-
-  initApp();
 })
