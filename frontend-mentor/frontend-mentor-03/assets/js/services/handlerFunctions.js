45f3c7a9def76fceeced711be7b8491ff7192673
will26developer <will.mart.tria.0304@gmail.com>
2026-04-05 21:13:30 +0200

    new changes in frontend-mentor-03

--- frontend-mentor/frontend-mentor-03/assets/js/services/handlerFunctions.js
+++ frontend-mentor/frontend-mentor-03/assets/js/services/handlerFunctions.js
@@ -3,45 +3,44 @@ import elements from "../elements/elements.js";
 import utilFunctions from "./utilFunctions.js";
 
 const debouncedSearch = utilFunctions.debounce((value) => {
-  stateFunctions.setFilterName(value);
+    stateFunctions.setFilterName(value);
 }, 300);
 
-
 const handlerFunctions = {
-    handlerBtnToggleMode: e => {
+    handlerBtnToggleMode: (e) => {
         const theme = stateFunctions.getState().theme;
         if (theme === "light-mode") {
-            elements.iconTheme.classList.remove("fa-regular","fa-sun","fa-moon");
+            elements.iconTheme.classList.remove("fa-regular", "fa-sun", "fa-moon");
             elements.iconTheme.className = "fa-regular fa-sun";
             elements.spanTheme.textContent = "Light Mode";
-            elements.body.classList.remove("light-mode","dark-mode");
+            elements.body.classList.remove("light-mode", "dark-mode");
             elements.body.classList.add("dark-mode");
             stateFunctions.saveTheme("dark-mode");
         } else {
-            elements.iconTheme.classList.remove("fa-regular","fa-sun","fa-moon");
+            elements.iconTheme.classList.remove("fa-regular", "fa-sun", "fa-moon");
             elements.iconTheme.className = "fa-regular fa-moon";
             elements.spanTheme.textContent = "Dark Mode";
-            elements.body.classList.remove("light-mode","dark-mode");
+            elements.body.classList.remove("light-mode", "dark-mode");
             elements.body.classList.add("light-mode");
             stateFunctions.saveTheme("light-mode");
         }
     },
-    handlerInputForm: e => {
+    handlerInputForm: (e) => {
         e.preventDefault();
         debouncedSearch(e.target.value);
     },
-    handlerSelectRegion: e => {
+    handlerSelectRegion: (e) => {
         stateFunctions.setFilterRegion(e.target.value);
     },
-    handleCountryCardClick: e => {
+    handleCountryCardClick: (e) => {
         let element = e.target.closest("[data-choice]");
         if (!element) return;
         let countryName = element.dataset.choice;
         utilFunctions.navigate(countryName);
     },
-    handleBtnReturn: e => {
+    handleBtnReturn: (e) => {
         utilFunctions.navigate("");
-    }
-}
+    },
+};
 
-export default handlerFunctions; 
+export default handlerFunctions;
