0359d2c87d4fa11d832554504134e34ce4794237
will26developer <will.mart.tria.0304@gmail.com>
2026-04-22 03:32:45 +0200

    changes in portfolio project

---
+++ react-projects/portfolio/src/data/projects.js
@@ -0,0 +1,53 @@
+import frontend1 from "../assets/projects-img/frontend-mentor-1.jpg";
+import frontend2 from "../assets/projects-img/frontend-mentor-2.jpg";
+import frontend3 from "../assets/projects-img/frontend-mentor-4.jpg";
+import frontend4 from "../assets/projects-img/frontend-mentor-5.jpg";
+import frontend5 from "../assets/projects-img/frontend-mentor-6.jpg";
+import frontend6 from "../assets/projects-img/frontend-mentor-7.jpg"; 
+import frontend7 from "../assets/projects-img/frontend-mentor-8.jpg";
+import frontend8 from "../assets/projects-img/frontend-mentor-9.jpg";
+import fullstack1 from  "../assets/projects-img/frontend-mentor-3.jpg";
+
+
+export const projects = {
+    frontend:[
+        {
+            img:frontend1,
+            url:"https://web-projects-q7k6.vercel.app/",
+        },
+        {
+            img:frontend2,
+            url:"https://web-projects-f41l.vercel.app/",
+        },
+        {
+            img:frontend3,
+            url:"https://web-projects-inp1.vercel.app/",
+        },
+        {
+            img:frontend4,
+            url:"https://web-projects-47c4.vercel.app/",
+        },
+        {
+            img:frontend5,
+            url:"https://web-projects-6axj.vercel.app/",
+        },
+        {
+            img:frontend6,
+            url:"https://web-projects-pj5q.vercel.app/",
+        },
+        {
+            img:frontend7,
+            url:"https://web-projects-mjc6.vercel.app/",
+        },
+        {
+            img:frontend8,
+            url:"https://web-projects-jade.vercel.app/",
+        }
+    ],
+    fullstack:[
+        {
+            img:fullstack1,
+            url:"https://web-projects-onrq.onrender.com/"
+        }
+    ]
+}
--- react-projects/portfolio/src/pages/Archive.jsx
+++ react-projects/portfolio/src/pages/Archive.jsx
@@ -1,16 +1,23 @@
 import { useNavigate } from "react-router-dom";
-
+import { projects } from "../data/projects";
 export const Archive = () => {
   const navigate = useNavigate();
 
-  const handlerClickBackHome = e => [
-    navigate("/")
-  ]
+  const handlerClickBackHome = (e) => {
+    navigate("/");
+  };
   return (
     <section id="archive" className="section">
       <div className="container">
         <div className="container-btn">
-          <button type="button" className="btn-back" onClick={handlerClickBackHome}><i className="fa-solid fa-arrow-left"></i><span>Home</span></button>
+          <button
+            type="button"
+            className="btn-back"
+            onClick={handlerClickBackHome}
+          >
+            <i className="fa-solid fa-arrow-left"></i>
+            <span>Home</span>
+          </button>
         </div>
 
         <h2 className="section-label">02. Project Archive</h2>
@@ -18,13 +25,41 @@ export const Archive = () => {
           <div className="grid-title">
             <h3 className="title-grid">Statics and frontend projects</h3>
           </div>
+          <div className="grid-container">
+            {projects.frontend.map(({ img, url, id }) => {
+              return (
+                <div className="container-img" key={url}>
+                  <a href={url} target="_blank">
+                    <img
+                      src={img}
+                      alt={`frontend-${id + 1}`}
+                      className="img-hero"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
         </div>
         <div className="archive-grid">
           <div className="grid-title">
             <h3 className="title-grid">Fullstack Projects</h3>
           </div>
+           {projects.fullstack.map(({ img, url, id }) => {
+              return (
+                <div className="container-img" key={url}>
+                  <a href={url} target="_blank">
+                    <img
+                      src={img}
+                      alt={`frontend-${id + 1}`}
+                      className="img-hero"
+                    />
+                  </a>
+                </div>
+              );
+            })}
         </div>
       </div>
     </section>
   );
-}
+};
