e4d32e1ded8c425292bb910e94061762bd16db71
will26developer <will.mart.tria.0304@gmail.com>
2026-04-26 20:31:22 +0200

    new changes in  react-projects

--- react-projects/rest-api-countries-frontend/src/hooks/useRestApiCountries.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { RestApiCountriesContext } from "../context/RestApiCountriesContext";
-
-export const useRestApiCountries = () => useContext(RestApiCountriesContext);
--- react-projects/rest-countries-app/src/router/router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { apiController } from "../controllers/apiController.js";
-
-const router = express.Router();
-
-router.get("/countries",apiController);
-
-export default router;
