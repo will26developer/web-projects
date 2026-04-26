1d1fa8b6f97e3cdcd2d6935c8ce2b8ee2e2a7411
will26developer <will.mart.tria.0304@gmail.com>
2026-04-26 18:58:28 +0200

    new changes in api mono repo

--- apis/api-rest-countries/src/configs/setupMiddlewares.js
+++ apis/api-rest-countries/src/configs/setupMiddlewares.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 
 export const setupMiddlewares = app => {
-  app.use(cors());
+  app.use(cors({
+    origin: process.env.FRONTEND_URL
+  }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 }
