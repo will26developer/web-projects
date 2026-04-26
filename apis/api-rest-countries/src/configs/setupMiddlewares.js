fee87255afa923ebca8b6e26c9fa256e3c756c71
will26developer <will.mart.tria.0304@gmail.com>
2026-04-26 19:40:02 +0200

    new changes in api rest countries -

--- apis/api-rest-countries/src/configs/setupMiddlewares.js
+++ apis/api-rest-countries/src/configs/setupMiddlewares.js
@@ -2,9 +2,7 @@ import express from "express";
 import cors from "cors";
 
 export const setupMiddlewares = app => {
-  app.use(cors({
-    origin: process.env.FRONTEND_URL
-  }));
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 }
