6e7d3497e2bbcb019bc6640ed6c15ac3027d0da1
will26developer <will.mart.tria.0304@gmail.com>
2026-04-26 19:45:38 +0200

    new changes in api rest countries

--- apis/api-rest-countries/src/configs/setupMiddlewares.js
+++ apis/api-rest-countries/src/configs/setupMiddlewares.js
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 
 export const setupMiddlewares = app => {
-  app.use(cors());
+  app.use(cors({
+    origin:"https://web-projects-ms5r.vercel.app"
+  }));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 }
