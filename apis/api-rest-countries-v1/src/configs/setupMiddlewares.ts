64f9314afd982fa09301f95330e3aa8ffe18a50f
will26developer <will.mart.tria.0304@gmail.com>
2026-05-01 18:54:45 +0200

    new changes in api-rest-countries-v1

--- apis/api-rest-countries-v1/src/configs/setupMiddlewares.ts
+++ apis/api-rest-countries-v1/src/configs/setupMiddlewares.ts
@@ -1,10 +1,25 @@
-import express, { Application } from 'express';
+import express, { Application } from "express";
 import cors from "cors";
-import helmet from 'helmet';
+import helmet from "helmet";
 
-export const setupMiddlewares = (app:Application) => {
-    app.use(helmet());
-    app.use(cors());
-    app.use(express.json({limit:"10kb"}));
-    app.use(express.urlencoded({extended: true})); 
-}
+const allowedOrigins = ["https://web-projects-4jun.vercel.app"];
+
+export const setupMiddlewares = (app: Application) => {
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        // permitir requests sin origin (ej: Postman)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+          return callback(null, true);
+        }
+
+        return callback(new Error("Not allowed by CORS"));
+      },
+    }),
+  );
+  app.use(express.json({ limit: "10kb" }));
+  app.use(express.urlencoded({ extended: true }));
+};
